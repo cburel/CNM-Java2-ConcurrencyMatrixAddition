b89e00383b19e047b0e9a7544963645cef529d39
Celeste Burel <krycel.2.0@gmail.com>
2026-03-17 09:27:15 -0600

    implement threads in main

--- Main.java
+++ Main.java
@@ -6,8 +6,6 @@ starting place. It should be modified.
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -39,24 +37,33 @@ public class Main {
             // read the matrix from the file
             int[][] matrix = readInMatrix(rows, cols, inFile);
 
-            // DEBUG
+            // create empty result matrix
+            int[][] result = new int[rows][cols];
+
+            // create the threads
+            ThreadOperation.Quadrant[] quadrants = ThreadOperation.Quadrant.values();
+            Thread[] threads = new Thread[4];
+
+            // start the threads
+            for (int i = 0; i < quadrants.length; i++) {
+                ThreadOperation task = new ThreadOperation(matrix, matrix, result, quadrants[i]);
+                threads[i] = new Thread(task);
+                threads[i].start();
+            }
+
+            // join the threads
+            for (Thread t : threads) {
+                try {
+                    t.join();
+                } catch (InterruptedException e) {
+                    System.out.println("Thread was interrupted:");
+                    e.printStackTrace();
+                }
+            }
+
+            // output
             print2dArray(matrix);
         }
-
-        // instantiate four ThreadOperation objects. feed in 3 matrices and a quadrant
-        // indicator.
-
-        ArrayList<ThreadOperation> operations = new ArrayList<ThreadOperation>();
-        operations.addAll(Arrays.asList(
-                new ThreadOperation(null, null, null, 0),
-                new ThreadOperation(null, null, null, 0),
-                new ThreadOperation(null, null, null, 0),
-                new ThreadOperation(null, null, null, 0)));
-
-        // start all threads and join them
-
-        // print the summed matrix
-
     }
 
     public static int[][] readInMatrix(int rows, int columns, Scanner inFile) {
@@ -79,7 +86,7 @@ public class Main {
         // use sysout.printf to print matrix with rows and cols aligned
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                System.out.printf("%2d", matrix[i][j]);
+                System.out.printf("%4d", matrix[i][j]);
             }
             System.out.println();
         }
--- ThreadOperation.java
+++ ThreadOperation.java
@@ -1,65 +1,79 @@
 public class ThreadOperation extends Thread {
 
-    int[][] arr1;
-    int[][] arr2;
-    int[][] arr3;
-    int quadrant;
+    int[][] arr1, arr2, arr3;
+    Quadrant quadrant;
 
-    public enum Quadrants {
+    public enum Quadrant {
         UPPER_LEFT,
         UPPER_RIGHT,
         LOWER_LEFT,
         LOWER_RIGHT
     }
 
-    ThreadOperation(int[][] arr1, int[][] arr2, int[][] arr3, int quadrant) {
+    ThreadOperation(int[][] arr1, int[][] arr2, int[][] arr3, Quadrant quadrant) {
         this.arr1 = arr1;
         this.arr2 = arr2;
         this.arr3 = arr3;
         this.quadrant = quadrant;
     }
 
-    public static int[] getQuadrantIndexes(int rows, int cols, Quadrants quadrant) {
+    public static int[] getQuadrantIndexes(int rows, int cols, Quadrant quadrant) {
 
+        // assign indexes to: {start of row, end of row, start of col, end of col}
         int[] indexes = new int[4];
 
-        int numRows = rows;
-        int numCols = cols;
+        int halfRows = rows / 2;
+        int halfCols = cols / 2;
 
-        // split the matrix into 2 rows
-        if (getNumRowsEven(rows)) {
-            numRows = numRows / 2;
-        } else {
-            numRows = numRows / 2 + 1;
+        switch (quadrant) {
+            case UPPER_LEFT:
+                indexes[0] = 0; // start row
+                indexes[1] = halfRows - 1; // end row
+                indexes[2] = 0; // start col
+                indexes[3] = halfCols - 1; // end col
+                break;
+            case UPPER_RIGHT:
+                indexes[0] = 0;
+                indexes[1] = halfRows - 1;
+                indexes[2] = halfCols;
+                indexes[3] = cols - 1;
+                break;
+            case LOWER_LEFT:
+                indexes[0] = halfRows;
+                indexes[1] = rows - 1;
+                indexes[2] = 0;
+                indexes[3] = halfCols - 1;
+                break;
+
+            case LOWER_RIGHT:
+                indexes[0] = halfRows;
+                indexes[1] = rows - 1;
+                indexes[2] = halfCols;
+                indexes[3] = cols - 1;
+                break;
+
         }
-
-        // split the matrix into 2 cols
-        if (getNumColsEven(numCols)) {
-            numCols = numCols / 2;
-        } else {
-            numCols = numCols / 2 + 1;
-        }
-
-        // TODO: assign matrix parts to quadrants
 
         return indexes;
     }
 
-    public static boolean getNumRowsEven(int rows) {
-        if (rows % 2 == 0) {
-            return true;
-        }
-        return false;
-    }
-
-    public static boolean getNumColsEven(int cols) {
-        if (cols % 2 == 0) {
-            return true;
-        }
-        return false;
-    }
-
+    @Override
     public void run() {
+
+        int[] bounds = getQuadrantIndexes(arr1.length, arr1[0].length, this.quadrant);
+
+        // assigned indexes are: {start of row, end of row, start of col, end of col}
+        int startRow = bounds[0];
+        int endRow = bounds[1];
+        int startCol = bounds[2];
+        int endCol = bounds[3];
+
+        // add the matrix quadrants
+        for (int i = startRow; i <= endRow; i++) {
+            for (int j = startCol; j <= endCol; j++) {
+                arr3[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
 
     }
 }
